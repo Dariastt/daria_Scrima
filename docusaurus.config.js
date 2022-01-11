ee65b54ea7d2d7e89124a993168c518bc30e894a
Daria <daria.stanciulescu2007@gmail.com>
2022-01-11 19:44:46 +0200

    Modificare 10

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'daria_scrima',
   tagline: 'Sa invatam scrima!!',
   url: 'https://dariastt.github.io',
-  baseUrl: '/daria_scrima/',
+  baseUrl: '/daria_Scrima/',
 
 
   onBrokenLinks: 'throw',
